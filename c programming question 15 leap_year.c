73e7ee4b72fa2d104bca5b2f82a1037d5b72792c
Vansh Arora <vansharora803@gmail.com>
2021-05-30 18:09:08 +0530

    Create c programming question 15 leap_year.c

---
+++ c programming question 15 leap_year.c
@@ -0,0 +1,26 @@
+// C program to find a year is leap year or not
+
+#include<stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("enter the year");
+    scanf("%d",&year);
+
+    if(year % 100 == 0)
+    {
+        if(year % 400 == 0)
+            printf("leap year");
+        else    
+            printf("not a leap year");
+    }
+    else
+    {
+        if(year % 4 == 0)
+            printf("leap year");
+        else
+            printf("not a leap year");
+    }
+}
